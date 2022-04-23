24e338a354dda4b2711b72c2b04828c68daf6098
moha5145 <mms.jimma@gmail.com>
2022-04-23 13:26:03 +0200

    add-form vanila.js

---
+++ assets/js/form.js
@@ -0,0 +1,33 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+  console.log("loded");
+  $.querySelector("#login").addEventListener("click", () => {
+    console.log("clicked");
+    $.querySelector(".modal").classList.remove("hidden");
+    $.querySelector("body").classList.add("disable");
+    $.querySelector(".main-nav").classList.add("hide-main-nav");
+  });
+
+  $.querySelector("#icon-times").addEventListener("click", () => {
+    console.log("clicked");
+    $.querySelector(".main-nav").classList.remove("hide-main-nav");
+    $.querySelector(".modal").classList.add("hidden");
+    $.querySelector("body").classList.remove("disable");
+  });
+
+  $.querySelector("#contactForm").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const data = {
+      firstname: $.querySelector("#firstname").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      message: $.querySelector("#description").value,
+    };
+
+    const response = await axios.post("http://localhost:3000/form", data);
+    alert(response.data.message);
+    console.log(response.data, data);
+  });
+});
